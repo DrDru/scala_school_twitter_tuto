0ca7e2027767ae207748e863bf97e19a687e185d
DrDru <DrDru@users.noreply.github.com>
2018-05-05 14:38:34 +0100

    my_first_stream

---
+++ my_first_stream.scala
@@ -0,0 +1,5 @@
+// This is my first stream
+
+val my_array = Array[String]("one", "two", "three", "four")
+
+def anotherStream(number: Int, an_array : Array[String]): Stream[String] = Stream.cons(an_array(number), anotherStream(number + 1, an_array))
