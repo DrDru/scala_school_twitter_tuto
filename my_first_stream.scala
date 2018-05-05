7a7ac3f9d1289df09aae1afb5422425d0afb7d20
th v <DrDru@users.noreply.github.com>
2018-05-05 14:58:07 +0100

    add remainder operator to avoid out of bouond erros

--- my_first_stream.scala
+++ my_first_stream.scala
@@ -2,4 +2,4 @@
 
 val my_array = Array[String]("one", "two", "three", "four")
 
-def anotherStream(number: Int, an_array : Array[String]): Stream[String] = Stream.cons(an_array(number), anotherStream(number + 1, an_array))
+def anotherStream(number: Int, an_array : Array[String]): Stream[String] = Stream.cons(an_array(number % an_array.length), anotherStream(number + 1, an_array))
